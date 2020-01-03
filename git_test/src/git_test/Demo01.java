afe2917596f855a34a068eaaf0ae546abd2fba2a
ksana4004 <ksana404@163.com>
2020-01-03 11:53:32 +0800

    ms:四次

--- git_test/src/git_test/Demo01.java
+++ git_test/src/git_test/Demo01.java
@@ -4,6 +4,9 @@ public class Demo01 {
 	public static void main(String[] args) {
 	
 		System.out.println("hello,word!");
+		System.out.println("test: v1");
+		System.out.println("test: v3");
+		
 		
 		
 	}
